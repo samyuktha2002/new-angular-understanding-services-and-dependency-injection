a8e82036f6898375db6a7321570b621facea6ad1
SAMYUKTHA KRP <krpsamyuktha2002@gmail.com>
2026-05-19 22:34:38 +0530

    Outsourcing and reusing logic with services

--- src/app/tasks/tasks-list/tasks-list.component.ts
+++ src/app/tasks/tasks-list/tasks-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, inject, signal, computed} from '@angular/core';
 
 import { TaskItemComponent } from './task-item/task-item.component';
 import { TasksService } from '../tasks.service';
@@ -11,9 +11,20 @@ import { TasksService } from '../tasks.service';
   imports: [TaskItemComponent],
 })
 export class TasksListComponent {
-  private tasksServive = inject(TasksService);
+  private tasksService = inject(TasksService);
   selectedFilter = signal<string>('all');
-  tasks = this.tasksServive.allTasks;
+  tasks = computed(() => {
+    switch(this.selectedFilter()){
+      case 'open':
+        return this.tasksService.allTasks().filter((task) => task.status === 'OPEN');
+      case 'in-progress':  
+        return this.tasksService.allTasks().filter((task) => task.status === 'IN_PROGRESS');
+      case 'done':
+        return this.tasksService.allTasks().filter((task) => task.status === 'DONE');
+      default:
+        return this.tasksService.allTasks();    
+    }
+  });
 
   onChangeTasksFilter(filter: string) {
     this.selectedFilter.set(filter);
